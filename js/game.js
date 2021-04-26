409d28af57c89a5eea830c858c6727759c03e729
InfinitydeGuzma <>
2021-04-26 17:20:31 +0000

    Added js folder

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 /* global phaser */
 
 const config = {
-type: Phaser.AUTO,
-width: 1920,
-height: 1080,
-backgroundColor: 0x5f6e7a
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
 }
 const game = new Phaser.Game(config)
 console.log(game)
