e2ab296beb351c7043c14d97a921ae3195213873
InfinitydeGuzma <>
2021-04-29 02:19:56 +0000

    Added comments and icons to the files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global phaser */
 
+// Copyright (c) 2021 Infinity de Guzman All rights reserved
+//
+// Created by Infinity de Guzman
+// Created on April 2021
+// This is the Phaser3 configuration file
+
+//* Game Scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // Set backgroundColor
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 const game = new Phaser.Game(config)
 console.log(game)
