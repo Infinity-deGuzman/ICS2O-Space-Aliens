3d37beb3692700191c0cb431386e3a0fd787bd99
InfinitydeGuzma <>
2021-05-05 01:46:36 +0000

    added a titleScene

--- js/game.js
+++ js/game.js
@@ -7,9 +7,11 @@
 // This is the Phaser3 configuration file
 
 import SplashScene from './splashScene.js'
+import TitleScene from './titleScene.js'
 
 // Our Game Scene
 const splashScene = new SplashScene()
+const titleScene = new TitleScene()
 
 //* Game Scene */
 const config = {
@@ -36,6 +38,7 @@ const game = new Phaser.Game(config)
 // Load scenes
 // NOTE: remember any "key" is global and CANNOT be reused!
 game.scene.add('splashScene', splashScene)
+game.scene.add('titleScene', titleScene)
 
 // Start title
 game.scene.start('splashScene')
---
+++ js/titleScene.js
@@ -0,0 +1,30 @@
+/* global phaser */
+
+// Copyright (c) 2021 Infinity de Guzman All rights reserved
+//
+// Created by Infinity de Guzman
+// Created on May 2021
+// This is the Title Scene
+
+class TitleScene extends Phaser.Scene {
+  constructor () {
+    super({key: 'titleScene'})
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Title Scene')
+  }
+
+  create (data) {
+  }
+
+  update (time, delta) {
+    this.scene.switch('titleScene')
+  }
+}
+
+export default TitleScene
