bfd526e1c704f0b51fe1ca35a4ff180f7f31bdf8
InfinitydeGuzma <>
2021-06-03 02:03:59 +0000

    changed tabs to spaces

--- js/menuScene.js
+++ js/menuScene.js
@@ -25,7 +25,7 @@ class MenuScene extends Phaser.Scene {
   }
 
   create (data) {
-		this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground').setScale(2.75)
+    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground').setScale(2.75)
     this.menuSceneBackgroundImage.x = 1920 / 2
     this.menuSceneBackgroundImage.y = 1080 / 2
 
