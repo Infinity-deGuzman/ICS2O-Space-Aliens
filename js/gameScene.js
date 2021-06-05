1eab6b86534a9ed66b455b4d73726ed571287998
InfinitydeGuzma <>
2021-06-05 03:21:27 +0000

    does a game over function

--- js/gameScene.js
+++ js/gameScene.js
@@ -45,6 +45,7 @@ class GameScene extends Phaser.Scene {
 		// sound files
     this.load.audio('laser', 'assets/laser1.wav')
     this.load.audio('explosion', 'assets/barrelExploding.wav')
+    this.load.audio('bomb', 'assets/bomb.wav')
   }
 
   create (data) {
@@ -68,10 +69,21 @@ class GameScene extends Phaser.Scene {
       missileCollide.destroy()
       this.sound.play('explosion')
       this.score = this.score + 1
-      this.scoreText.setText('Score: '      this.score.toString())
+      this.scoreText.setText('Score: ' + this.score.toString())
       this.createAlien()
       this.createAlien()
 		}.bind (this))
+
+    //collision between ships and aliens
+    this.physics.add.collider(this.ship, this.alienGroup, function(shipCollide, alienCollide) {
+      this.sound.play('bomb')
+      this.physics.pause()
+      alienCollide.destroy()
+      shipCollide.destroy()
+      this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over!\nClick to play again.', this.gameOverTextStyle).setOrigin(0.5)
+      this.gameOverText.setInteractive({ useHandCursor: true })
+      this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
+    }.bind(this))
   }
 
   update (time, delta) {
